d06445f04709b2e38edc71edb49dceff18f7ceaa
kavery-pm <shiny.kavery@gmail.com>
2023-06-21 22:39:03 +0200

    configured the store

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
+
 import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
 import App from './App.tsx'
 import './index.css'
+import store from './store/index.tsx'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+    </Provider>
 )
