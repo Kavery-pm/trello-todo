77e40de58cb664812b798fdde19f88157f78eaa0
kavery-pm <shiny.kavery@gmail.com>
2023-06-21 22:58:45 +0200

    added the id generator and the sliceList

---
+++ src/idGenerator.ts
@@ -0,0 +1,8 @@
+export const idGenerator = () => {
+  let randomString = '';
+ let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < 8; i++) {
+    randomString += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return randomString;
+}
