e5a5673e3f1e07dffb3416136f081e90c7b1e9d0
kavery-pm <shiny.kavery@gmail.com>
2023-06-21 22:42:09 +0200

    added the global component using styled components

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Board } from './components/Board'
+import Global from './styles/global'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +12,7 @@ function App() {
     <>
      <h1>Todo list</h1>
      <Board/>
+     <Global/>
     </>
   )
 }
