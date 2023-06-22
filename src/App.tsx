f6f25b016ac915b74b96d602c3dc62619d81f1a9
kavery-pm <shiny.kavery@gmail.com>
2023-06-22 10:12:39 +0200

    added the config modal

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css'
 import { Board } from './components/Board'
 import Global from './styles/global'
 import { NewItemModal } from './components/NewItemModal'
+import { ConfigModal } from './components/ConfigModal'
 
 function App() {
  
@@ -11,6 +12,7 @@ function App() {
     <>
      <h1>Todo list</h1>
      <NewItemModal/>
+     <ConfigModal/>
      <Board/>
      <Global/>
     </>
