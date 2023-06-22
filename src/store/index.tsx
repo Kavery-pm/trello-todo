f7a53789ba3643d0b64fc66dc811a001bc41d9cd
kavery-pm <shiny.kavery@gmail.com>
2023-06-22 09:43:40 +0200

    fixed the build error

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import configReducer from './sliceConfig'
+import listsReducer from './sliceLists'
 const store = configureStore({
     reducer: {
         config: configReducer,
+lists: listsReducer
     }
   })
   export default store;
